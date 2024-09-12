9a61413e329b2fc8e3b29a5d7530964f0a23c93f
Acerbar <acergood1@yandex.ru>
2024-09-12 14:24:30 +0300

    d

--- src/components/sections/feedback/feedback.jsx
+++ src/components/sections/feedback/feedback.jsx
@@ -66,7 +66,7 @@ export default function Feedback() {
     setIsSubmitted(true);
     if (validateForm()) {
       try {
-        const response = await fetch('/server.js/api/send-email', {
+        const response = await fetch('server.js', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({ name, email, message, phone: number }),
