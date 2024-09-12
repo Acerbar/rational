297c7bbfca2e5a8bc327bfc53890c2f469d3df41
Acerbar <acergood1@yandex.ru>
2024-09-12 13:45:37 +0300

    b

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-import express from 'express';
+// import express from 'express';
 import { createTransport } from 'nodemailer';
-import cors from 'cors';
+// import cors from 'cors';
 
-const app = express();
-const PORT = 5000;
+// const app = express();
+// const PORT = 5000;
 
 
-app.use(express.json());
-app.use(cors());
+// app.use(express.json());
+// app.use(cors());
 
 const transporter = createTransport({
   host: "smtp.gmail.com",
