98c2521f13d7f3c97885d37001ee9afb049aaa81
Acerbar <114619445+Acerbar@users.noreply.github.com>
2024-09-13 11:57:17 +0300

    Update prefElemData.js

--- src/components/sections/prefs/prefElemData.js
+++ src/components/sections/prefs/prefElemData.js
@@ -4,22 +4,22 @@ export const prefElemsContentEn = [
     {
     id: 1,
     title: "> we bear the costs of maintenance of system infrastructure",
-    image: "/test.jpg",
+    image: "./test.jpg",
     },
     {
     id: 2,
     title: "> you can gain access to our services from anywhere in the world via the Internet",
-    image: "/access.jpg",
+    image: "./access.jpg",
     },
     {
     id: 3,
     title: "> cloud technologies with backups prevent your data from being hacked and corrupted",
-    image: "/dataProtection.jpg",
+    image: "./dataProtection.jpg",
     },
     {
     id: 4,
     title: "> you can quickly change the volume and character of services and use only what you need at the moment",
-    image: "/serviceVolume.jpg",
+    image: "./serviceVolume.jpg",
     },
     ]
 
@@ -27,22 +27,22 @@ export const prefElemsContentSr = [
         {
         id: 1,
         title: "> Mi snosimo troškove održavanja sistemske infrastrukture",
-        image: "/test.jpg",
+        image: "./test.jpg",
         },
         {
         id: 2,
         title: "> Možete dobiti pristup našim uslugama sa bilo kog mesta u svetu putem Interneta",
-        image: "/access.jpg",
+        image: "./access.jpg",
         },
         {
         id: 3,
         title: "> Cloud tehnologije sa rezervnim kopijama sprečavaju hakovanje i oštećenje vaših podataka",
-        image: "/dataProtection.jpg",
+        image: "./dataProtection.jpg",
         },
         {
         id: 4,
         title: "> Možete brzo promeniti obim i karakter usluga i koristiti samo ono što vam je trenutno potrebno",
-        image: "/serviceVolume.jpg",
+        image: "./serviceVolume.jpg",
         },
 
     ]
